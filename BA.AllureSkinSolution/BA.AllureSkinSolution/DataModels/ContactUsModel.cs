bc1c907236cfca781b2f5075d52e8ae50f9d0f0b
Aftab Ahmed <aaftab1441@gmail.com>
2024-12-05 13:49:35 -0500

    Allure skin solution

---
+++ BA.AllureSkinSolution/BA.AllureSkinSolution/DataModels/ContactUsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BA.AllureSkinsolution.Adapters;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace BA.AllureSkinsolution.DataModels
+{
+    public class ContactUsModel
+    {
+        [Required]
+        public string ZEq6Upn5d6 { get; set; }
+        [Required]
+        public string Y9xItYxUUA { get; set; }
+        [Required]
+        public string K7pmWqXbaV { get; set; }
+        public string kxCJgcNxcc { get; set; }
+        public List<IFormFile> Attachments { get; set; }
+        [Required]
+        public string ZPqL6w6Ao3 { get; set; }
+        public string Page { get; set; }
+    }
+}
---
+++ BA.AllureSkinSolution/BA.AllureSkinSolution/Pages/contact.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using BA.AllureSkinsolution.DataModels;
+using BA.AllureSkinsolution.Adapters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using BA.Common.Services;
+using reCAPTCHA.AspNetCore;
+
+namespace BA.AllureSkinsolution
+{
+    public class contactModel : PageModel
+    {
+        private readonly IConfiguration _config;
+        private readonly ISmtpService _smtpService;
+        private readonly IRecaptchaService _recaptcha;
+
+        [BindProperty]
+        public ContactUsModel contactUsModel { get; set; }
+
+        public contactModel(IConfiguration config, ISmtpService smtpService, IRecaptchaService recaptcha)
+        {
+            _config = config;
+            _smtpService = smtpService;
+            _recaptcha = recaptcha;
+        }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                contactUsModel.Page = "Contact Us";
+                RecaptchaResponse recaptcha = await _recaptcha.Validate(Request);
+                if (!recaptcha.success)
+                {
+                    ModelState.AddModelError("Recaptcha", "There was an error validating the Recaptcha code.  Please try Again!");
+                    return Page();
+                }
+                else
+                {
+                    ContactUsAdapter contactUs = new ContactUsAdapter(_config, _smtpService);
+                    await contactUs.CreateAndSendEmail(contactUsModel);
+                    return RedirectToPage("/ThankYou");
+                }
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
